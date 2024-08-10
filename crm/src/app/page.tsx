d12ad7c3aca6745ebfebe7144e925da27f8a1995
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-08-10 10:01:50 +0200

    add statusbtns

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,8 +1,13 @@
+import StatusLabel, { Status } from "./components/active-label";
 
 export default function Home() {
   return (
     <main>
-      <h1>homepage</h1>
+      <h1 className="text-9xl">homepage</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspend}>Suspend</StatusLabel>
     </main>
   );
 }
---
+++ crm/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

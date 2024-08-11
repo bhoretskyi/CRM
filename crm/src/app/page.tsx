04a1a11d60da0693264e2f78ef8ea0056580e4cd
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-08-11 20:52:14 +0200

    start imput field

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { headers } from "next/headers";
 import StatusLabel, { Status } from "./components/active-label";
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-9xl">homepage</h1>
+      <h1 className="text-2xl">homepage{new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>

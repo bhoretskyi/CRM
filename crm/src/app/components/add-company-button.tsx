bc1ad95ac8180cdc647e76430dbd2a8fb1911a1d
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-08-12 19:27:57 +0200

    add modal

---
+++ crm/src/app/components/add-company-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState } from "react";
+import Button from "@/app/components/button";
+import dynamic from "next/dynamic";
+// import CompanyFormModal from "@/app/components/company-form-modal";
+const CompanyFormModal = dynamic(() => import("./company-form-modal"), {
+  ssr: false,
+});
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,15 +1,13 @@
 import { headers } from "next/headers";
-import StatusLabel, { Status } from "./components/active-label";
+// import StatusLabel, { Status } from "./components/active-label";
+import AddCompanyButton from "./components/add-company-button";
 
 export default function Home() {
   console.log(headers());
   return (
     <main>
       <h1 className="text-2xl">homepage{new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspend}>Suspend</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }

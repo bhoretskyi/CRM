094b209f715956cb962336e22edf8fdc8434b774
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-08-27 13:47:51 +0200

    add sidebar

--- crm/src/app/(admin)/companies/page.tsx
+++ crm/src/app/(admin)/companies/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../components/header";
-import Toolbar from "../components/footbar";
-import SearchInput from "../components/search-input";
-import AddCompanyButton from "../components/add-company-button";
-import CompanyTable from "../components/company-table";
-import CompanyRow from "../components/company-row";
+import Header from "@/app/components/header";
+import Toolbar from "@/app/components/footbar";
+import SearchInput from "@/app/components/search-input";
+import AddCompanyButton from "@/app/components/add-company-button";
+import CompanyTable from "@/app/components/company-table";
+import CompanyRow from "@/app/components/company-row";
 import { Status } from "@/app/components/status-label";
 export interface PageProps {}
 export default function Page({}: PageProps) {
--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -3,7 +3,6 @@ import { headers } from "next/headers";
 import AddCompanyButton from "./components/add-company-button";
 
 export default function Home() {
-  console.log(headers());
   return (
     <main>
       <h1 className="text-2xl">homepage{new Date().toTimeString()}</h1>
